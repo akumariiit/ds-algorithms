12524620132da7726ba0cd8119475e61ca3c042c
Amit Kumar <amitkumar@expedia.com>
2016-12-30 10:32:11 +0530

    Add files via upload

---
+++ stacks/StackReverse.java
@@ -0,0 +1,35 @@
+package stacks;
+
+/**
+ * Created by amitkumar on 19/12/16.
+ */
+
+/**
+ * if stack has 1, 2, 3, 4
+ * result should be 4, 3, 2, 1
+ */
+public class StackReverse {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(4);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        int n = stack.getSize();
+        for (int i = 1; i < n; i++) {
+            slide(1, n-i+1, stack);
+        }
+        stack.print();
+    }
+
+    private static void slide(int start, int end, Stack stack) {
+        if (end == start)
+            return;
+        int first = stack.pop();
+        int second = stack.pop();
+        stack.push(first);
+        slide(start+1, end, stack);
+        stack.push(second);
+    }
+}
